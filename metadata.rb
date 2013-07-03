b6bc53db6c29d3d7d1c3f5801410b30d2a05a736
dueckes <matthew@ueckerman.net>
2013-07-03 15:29:08 +1000

    Declared Ubuntu 12.04 support

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,4 @@ description      'Installs/Configures freenx-server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.0'
 
+supports "ubuntu", "= 12.04"
